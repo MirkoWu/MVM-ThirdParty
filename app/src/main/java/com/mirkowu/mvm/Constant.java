45e29849fda496fb45fdfa24f5d76b4f536e55be
wumeng1 <1yw@Mirko>
2022-06-17 16:50:57 +0800

    #update 更新demo

--- app/src/main/java/com/mirkowu/mvm/Constant.java
+++
@@ -1,5 +0,0 @@
-package com.mirkowu.mvm;
-
-public class Constant {
-    public static final String FILE_SAVE_DIR = "Mirko";
-}
--- app/src/main/java/com/mirkowu/mvm/MainActivity.kt
+++ app/src/main/java/com/mirkowu/mvm/MainActivity.kt
@@ -1,27 +1,16 @@
 package com.mirkowu.mvm
 
-import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import android.view.View
-import androidx.fragment.app.Fragment
-import com.mirkowu.lib_base.adapter.BaseFragmentPagerAdapter
 import com.mirkowu.lib_base.mediator.EmptyMediator
 import com.mirkowu.lib_base.util.bindingView
 import com.mirkowu.lib_bugly.BuglyManager
-import com.mirkowu.lib_bugly.UpgradeDialog
-import com.mirkowu.lib_util.LogUtil
+import com.mirkowu.lib_util.ktxutil.click
 import com.mirkowu.lib_util.utilcode.util.BarUtils
+import com.mirkowu.lib_util.utilcode.util.ToastUtils
 import com.mirkowu.lib_webview.CommonWebActivity
 import com.mirkowu.mvm.base.BaseActivity
-import com.mirkowu.mvm.mvc.MVCActivity
-import com.mirkowu.mvm.mvp.MVPActivity
-import com.mirkowu.mvm.mvvm.MVVMActivity
-import com.mirkowu.mvm.recycelerview.GridListActivity
-import com.mirkowu.mvm.mvvm.viewbinding.DataBindingActivity
-import com.mirkowu.mvm.mvvm.viewbinding.DataBindingFragment
-import com.mirkowu.mvm.webview.WebActivity
 
 class MainActivity : BaseActivity<EmptyMediator>() {
     companion object {
@@ -32,7 +21,6 @@ class MainActivity : BaseActivity<EmptyMediator>() {
     }
 
     val binding by bindingView(com.mirkowu.mvm.databinding.ActivityMainBinding::inflate)
-    lateinit var pagerAdapter: BaseFragmentPagerAdapter
 
     override fun initMediator(): EmptyMediator {
         return super.initMediator()
@@ -46,81 +34,46 @@ class MainActivity : BaseActivity<EmptyMediator>() {
     override fun getLayoutId() = R.layout.activity_main
 
     override fun initialize() {
-        LogUtil.d("点击 跳过 initialize")
-        pagerAdapter =
-            BaseFragmentPagerAdapter(
-                supportFragmentManager,
-                mutableListOf<Fragment>(
-                    DataBindingFragment.newInstance(),
-//                    DataBindingFragment.newInstance(),
-                )
-            )
-
-        binding.vpHome.apply {
-            adapter = pagerAdapter
-            isScroll = true
-            offscreenPageLimit = pagerAdapter.count
+        binding.btnJumpMvm.click {
+            CommonWebActivity.start(this,"","https://github.com/MirkoWu/MVM")
         }
-        val PERMISSION_STORAGE = arrayOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.READ_PHONE_STATE
-        )
-
-        BuglyManager.checkUpgrade { hasNewVersion, upgradeInfo ->
-            Log.e("BuglyManager", "setUpgradeListener:   upgradeInfo=$upgradeInfo")
-            if (upgradeInfo != null) {
-                UpgradeDialog.show(supportFragmentManager, upgradeInfo)
+        binding.btnBuglyUpgrade.click {
+            showLoadingDialog("检查新版本")
+            BuglyManager.checkUpgrade { hasNewVersion, upgradeInfo ->
+                Log.e("BuglyManager", "setUpgradeListener:   upgradeInfo=$upgradeInfo  ")
+                hideLoadingDialog()
+                if (upgradeInfo != null) {
+                    com.mirkowu.lib_bugly.UpgradeDialog.show(supportFragmentManager, upgradeInfo)
+                } else {
+                    ToastUtils.showShort("当前已是最新版本!")
+                }
             }
         }
+        binding.btnUpgrade.click {
+            // BuglyManager.checkUpgrade(true, false)
+            val url =
+                "https://outexp-beta.cdn.qq.com/outbeta/2021/06/18/commirkowumvm_1.0.1_56987f9a-fb39-56d5-9ac4-a4c055633672.apk"
+            BuglyManager.checkUpgrade { hasNewVersion, upgradeInfo ->
+                Log.e("BuglyManager", "setUpgradeListener:   upgradeInfo=$upgradeInfo  ")
 
 
-        LogUtil.d("点击 跳过 initialize end---")
+                if (upgradeInfo != null) {
+                    com.mirkowu.lib_bugly.UpgradeDialog.show(supportFragmentManager, upgradeInfo)
+                } else {
+                    ToastUtils.showShort("当前已是最新版本!")
+                }
+            }
+
+        }
+
+        binding.btnCrash.click {
+            throw RuntimeException("测试BUG")
+        }
 
     }
 
-    fun webLocalClick(view: View?) {
-//        MVCActivity.start(this)
-//        WebActivity.start(context, "ces", "file:///android_asset/test.html")
-        WebActivity.start(context, "ces", "file:///android_asset/jsbridge_test.html")
-//        WebActivity.start(context, "ces", "http://www.baidu.com")
-        Log.d("WebActivity", "start: ")
-    }
-
-    fun webNetClick(view: View?) {
-//        CommonWebActivity.start(context, "ces", "http://www.baid")
-//        WebViewActivity.start(context, "ces", "https://x5.tencent.com/docs/questions.html")
-        CommonWebActivity.start(
-            context,
-            "标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题标题",
-            "http://www.baidu.com/"
-        )
-        Log.d("WebActivity", "start: ")
-    }
-
-    fun mvcClick(view: View?) {
-        MVCActivity.start(this)
-    }
-
-    fun mvpClick(view: View?) {
-        MVPActivity.start(this)
-    }
-
-    fun mvvmClick(view: View?) {
-        MVVMActivity.start(this)
-    }
-
-    fun bindingClick(view: View?) {
-        startActivity(Intent(this, DataBindingActivity::class.java))
-    }
-
-    fun listClick(view: View?) {
-//        LinearListActivity.start(this)
-        GridListActivity.start(this)
-    }
 
     override fun onDestroy() {
-        pagerAdapter.clear()
         BuglyManager.removeUpgradeListener()
         super.onDestroy()
     }
